71d0e095ff42b9977a18cf6455067d885cf74eda
ankushdogradev <ankushdogradev@gmail.com>
2021-11-25 11:51:37 +0530

    Adding only theme

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-
-gulp.task('sass', function () {
-    return gulp.src('scss/global.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('theme/assets'))
-});
